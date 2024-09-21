5cbf6206a8f85c075e82e228de5845c54a3db0b5
Jagyanjit <ece.22beci74@silicon.ac.in>
2024-09-21 23:00:01 +0530

    Create Themes.js

---
+++ Context/Themes.js
@@ -0,0 +1,11 @@
+export const DarkTheme = {
+  background: '#333',
+  text: '#fff',
+  button: '#444',
+};
+
+export const LightTheme = {
+  background: '#fff',
+  text: '#000',
+  button: '#ddd',
+};
